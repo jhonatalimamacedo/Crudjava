4bd10231f46e9aed1852f34d8db5a40e2b79c19e
jhonataLimaMacedo <jhonata.limamacedoti@gmail.com>
2023-08-18 19:33:04 -0300

    alteração

---
+++ src/main/java/com/jhonata/usuarioapi/Service/UsuarioService.java
@@ -0,0 +1,11 @@
+package com.jhonata.usuarioapi.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    public String getGreeting() {
+        return "Hello from the service!";
+    }
+}
